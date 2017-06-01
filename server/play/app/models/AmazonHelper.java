e5b1346807929fb8cf7c3711443b3a7e90e8649e
Douglas Selph <douglasselph@gmail.com>
2017-06-01 17:18:22 -0500

    trying to get amazon s3 to work on server

---
+++ server/play/app/models/AmazonHelper.java
@@ -0,0 +1,13 @@
+package models;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Download;
+import java.io.File;
+
+public class AmazonHelper  {
+
+
+
+}
+
