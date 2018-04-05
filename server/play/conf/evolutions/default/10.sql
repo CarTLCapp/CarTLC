8f7e635f65e5b63e88a2c148c17b0b7fbdf0bcec
Douglas Selph <douglasselph@gmail.com>
2018-04-05 07:45:49 -0500

    extra comma

--- server/play/conf/evolutions/default/10.sql
+++ server/play/conf/evolutions/default/10.sql
@@ -5,7 +5,7 @@
 CREATE TABLE secondary_technician (
     id                  int auto_increment primary key,
     entry_id            int default 0,
-    secondary_tech_id   int default 0,
+    secondary_tech_id   int default 0
 );
 
 # --- !Downs
