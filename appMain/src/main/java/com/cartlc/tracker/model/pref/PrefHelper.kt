2e69e6d86165f78c92cd906d04d482bda2b54222
Douglas Selph <douglasselph@gmail.com>
2019-02-20 09:49:38 -0600

    added comment

--- appMain/src/main/java/com/cartlc/tracker/model/pref/PrefHelper.kt
+++ appMain/src/main/java/com/cartlc/tracker/model/pref/PrefHelper.kt
@@ -352,6 +352,7 @@ class PrefHelper constructor(
 
     fun clearLastEntry() {
 
+        // TODO: this is debug temporary code:
         db.reportDebugMessage("clearLastEntry(): TRUCK#=$truckNumber, #PICTURES="
                 + db.tablePictureCollection.countPendingPictures())
 
